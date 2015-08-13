d703072d7349905b341da60a4e8a2ec4b8afc585
Alan Peabody <gapeabody@gmail.com>
2015-08-12 21:31:55 -0400

    Provide default formatter implementations for base type.
    Falling back to the Any type was resulting in a huge performance hit.

--- lib/ja_serializer/formatter.ex
+++ lib/ja_serializer/formatter.ex
@@ -10,3 +10,7 @@ end
 defimpl JaSerializer.Formatter, for: List do
   def format(list), do: Enum.map(list, &JaSerializer.Formatter.format/1)
 end
+
+defimpl JaSerializer.Formatter, for: [BitString, Integer, Float, Atom] do
+  def format(data), do: data
+end
