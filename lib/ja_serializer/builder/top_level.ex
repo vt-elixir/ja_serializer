476a1132365c33f55607b4d11e210a935d38c794
Vysakh Sreenivasan <diplomatv@gmail.com>
2015-10-04 02:08:51 +0530

    Implement pagination links
    As per the [json api spec on pagination
    links](http://jsonapi.org/format/#fetching-pagination), have a top
    level `links` object with keys
    
    - self
    - first
    - last
    - prev
    - next
    
    A [sample json picked from rails-api/ams]
    (https://github.com/rails-api/active_model_serializers/blob/master/docs/howto/add_pagination_links.md)
    
    ```json
    {
      "data": [
        {
          "type": "articles",
          "id": "3",
          "attributes": {
            "title": "JSON API paints my bikeshed!",
            "body": "The shortest article. Ever.",
            "created": "2015-05-22T14:56:29.000Z",
            "updated": "2015-05-22T14:56:28.000Z"
          }
        }
      ],
      "links": {
        "self": "http://example.com/articles?page[number]=3&page[size]=1",
        "first": "http://example.com/articles?page[number]=1&page[size]=1",
        "prev": "http://example.com/articles?page[number]=2&page[size]=1",
        "next": "http://example.com/articles?page[number]=4&page[size]=1",
        "last": "http://example.com/articles?page[number]=13&page[size]=1"
      }
    }
    ```
    
    Also Fixes https://github.com/AgilionApps/ja_serializer/issues/23
    
    Fix README

--- lib/ja_serializer/builder/top_level.ex
+++ lib/ja_serializer/builder/top_level.ex
@@ -3,6 +3,8 @@ defmodule JaSerializer.Builder.TopLevel do
 
   alias JaSerializer.Builder.ResourceObject
   alias JaSerializer.Builder.Included
+  alias JaSerializer.Builder.Link
+  alias JaSerializer.Builder.PaginationLinks
 
   defstruct [:data, :errors, :included, :meta, :links, :jsonapi]
 
@@ -15,9 +17,32 @@ defmodule JaSerializer.Builder.TopLevel do
     |> add_links(context)
   end
 
-  #TODO Add links for pagination etc
+  def add_links(tl, %{opts: %{page: _}} = context) do
+    data = tl.data
+    if is_list(data) do
+      links = data
+              |> hd
+              |> find_index_url
+              |> map_links(data, context)
+      Map.put(tl, :links, links)
+    else
+      tl
+    end
+  end
+
   def add_links(tl, _context), do: tl
 
   #TODO: Add meta
   def add_meta(tl, _context), do: tl
+
+  defp find_index_url(data) do
+    root_link = data.links |> hd
+    String.replace(root_link.href, ~r{\/\d+/?$}, "")
+  end
+
+  defp map_links(index_url, list, context) do
+    index_url
+    |> PaginationLinks.build(context.opts.page)
+    |> Enum.map(fn({type, url}) -> Link.build(context, type, url) end)
+  end
 end
