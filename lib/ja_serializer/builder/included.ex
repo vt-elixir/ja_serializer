c5755bdce86c7030914855ee00a2c45d35cc1a07
dgvncsz0f <dsouza@c0d3.xxx>
2016-03-21 19:48:31 -0300

    fix performance issue when serializer has includes
    The problem arises from using ++ in the loop [which is quadratic] and
    using lists for membership queries [which is linear]. The new code uses
    map and hashset which give us constant access/modification times.

--- lib/ja_serializer/builder/included.ex
+++ lib/ja_serializer/builder/included.ex
@@ -3,8 +3,17 @@ defmodule JaSerializer.Builder.Included do
 
   alias JaSerializer.Builder.ResourceObject
 
+  defp resource_key(resource) do
+    {resource.id, resource.type}
+  end
+
   def build(%{data: data} = context, primary_resources) when is_list(data) do
-    do_build(data, context, [], List.wrap(primary_resources))
+    known = List.wrap(primary_resources)
+    |> Enum.map(&resource_key/1)
+    |> Enum.into(HashSet.new)
+
+    do_build(data, context, %{}, known)
+    |> Map.values
   end
 
   def build(context, primary_resources) do
@@ -15,16 +24,13 @@ defmodule JaSerializer.Builder.Included do
 
   defp do_build([], _context, included, _known_resources), do: included
   defp do_build([struct | structs], context, included, known) do
-    context = Map.put(context, :data, struct)
-
-    new = context
-          |> relationships_with_include
-          |> Enum.flat_map(&resources_for_relationship(&1, context, included, known))
-          |> Enum.uniq(&({&1.id, &1.type}))
-          |> reject_known(included, known)
-
-    # Call for next struct
-    do_build(structs, context, (new ++ included), known)
+    context  = Map.put(context, :data, struct)
+    included = context
+    |> relationships_with_include
+    |> Enum.reduce(included, fn item, included ->
+      resources_for_relationship(item, context, included, known)
+    end)
+    do_build(structs, context, included, known)
   end
 
   defp resource_objects_for(structs, conn, serializer, fields) do
@@ -48,23 +54,24 @@ defmodule JaSerializer.Builder.Included do
 
   # Find resources for relationship & parent_context
   defp resources_for_relationship({_, name, opts}, context, included, known) do
-    context_opts = context[:opts]
-    new = apply(context.serializer, name, [context.data, context.conn])
-          |> List.wrap
-          |> resource_objects_for(context.conn, opts[:serializer], context_opts[:fields])
-          |> reject_known(included, known)
+    context_opts     = context[:opts]
+    {cont, included} = apply(context.serializer, name, [context.data, context.conn])
+    |> List.wrap
+    |> resource_objects_for(context.conn, opts[:serializer], context_opts[:fields])
+    |> Enum.reduce({[], included}, fn item, {cont, included} ->
+      key = resource_key(item)
+      if HashSet.member?(known, key) or Map.has_key?(included, key) do
+        {cont, included}
+      else
+        {[item.data | cont], Map.put(included, key, item)}
+      end
+    end)
 
     child_context = context
     |> Map.put(:serializer, opts[:serializer])
     |> Map.put(:opts, opts_with_includes_for_relation(context_opts, name))
 
-    new
-    |> Enum.map(&(&1.data))
-    |> do_build(child_context, (new ++ included), known)
-  end
-
-  defp reject_known(resources, included, primary) do
-    Enum.reject(resources, &(&1 in included || &1 in primary))
+    do_build(cont, child_context, included, known)
   end
 
   defp opts_with_includes_for_relation(opts, rel_name) do
