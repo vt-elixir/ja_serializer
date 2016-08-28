ab0e5bc729671e04bf617bf4236038c8e5445953
Alan Peabody <gapeabody@gmail.com>
2016-08-28 11:39:38 -0400

    Test preload behaviour.

--- test/ja_serializer/builder/top_level_test.exs
+++ test/ja_serializer/builder/top_level_test.exs
@@ -4,6 +4,14 @@ defmodule JaSerializer.Builder.TopLevelTest do
   defmodule PersonSerializer do
     use JaSerializer
     attributes [:first_name]
+
+    def preload(%TestModel.Person{} = data, _conn, _opts) do
+      %{data | last_name: "preloaded"}
+    end
+
+    def preload(data, _conn, _opts) do
+      Enum.map(data, &(%{&1 | last_name: "preloaded"}))
+    end
   end
 
   test "pagination with scrivener" do
@@ -41,5 +49,31 @@ defmodule JaSerializer.Builder.TopLevelTest do
     }
     assert %{meta: meta} = JaSerializer.Builder.TopLevel.build(context)
     assert meta == %{author: "Dohn Joe"}
+  end
+
+  test "preload hook is called when building individual records" do
+    p1 = %TestModel.Person{id: "p1", first_name: "p1"}
+    context = %{
+      data: p1,
+      conn: %Plug.Conn{query_params: %{}},
+      serializer: PersonSerializer,
+      opts: []
+    }
+    assert %{data: data} = JaSerializer.Builder.TopLevel.build(context)
+    assert data.data.last_name == "preloaded"
+  end
+
+  test "preload hook is called when building multiple records" do
+    p1 = %TestModel.Person{id: "p1", first_name: "p1"}
+    p2 = %TestModel.Person{id: "p2", first_name: "p2"}
+    context = %{
+      data: [p1, p2],
+      conn: %Plug.Conn{query_params: %{}},
+      serializer: PersonSerializer,
+      opts: []
+    }
+    assert %{data: [b1, b2]} = JaSerializer.Builder.TopLevel.build(context)
+    assert b1.data.last_name == "preloaded"
+    assert b2.data.last_name == "preloaded"
   end
 end
