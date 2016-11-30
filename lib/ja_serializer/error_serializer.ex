83fc8e5ae17d907320fdc844bf4253078c4bfd25
Neil Calabroso <ncalabroso@gmail.com>
2016-11-30 12:25:04 +0800

    Put `jsonapi version` in error responses
    Put the `jsonapi version` in error responses to be more consistent with the non-error responses that include it.

--- lib/ja_serializer/error_serializer.ex
+++ lib/ja_serializer/error_serializer.ex
@@ -9,24 +9,25 @@ defmodule JaSerializer.ErrorSerializer do
   See http://jsonapi.org/format/#error-objects for more on errors.
   """
 
+  alias JaSerializer.Builder.TopLevel
+  alias JaSerializer.Formatter
+
   def format(error), do: format(error, %{})
   def format(error, conn), do: format(error, conn, [])
   def format(errors, _conn, _opts) when is_list(errors) do
-    errors
-    |> Enum.map(&format_one/1)
-    |> as_json
+    errors = errors |> Enum.map(&format_one/1)
+    %TopLevel{errors: errors} |> Formatter.format
   end
   def format(error, _conn, _opts) do
-    error
+    errors = error
     |> format_one
     |> List.wrap
-    |> as_json
+
+    %TopLevel{errors: errors} |> Formatter.format
   end
 
   @error_fields ~w(id links about status code title detail source meta)a
   defp format_one(error) do
     Dict.take(error, @error_fields)
   end
-
-  defp as_json(errors), do: %{"errors" => errors}
 end
--- lib/ja_serializer/formatter/top_level.ex
+++ lib/ja_serializer/formatter/top_level.ex
@@ -1,22 +1,31 @@
 defimpl JaSerializer.Formatter, for: JaSerializer.Builder.TopLevel do
   alias JaSerializer.Formatter.Utils
 
-  def format(struct) do
-    %{"jsonapi" =>  %{"version" => "1.0"}}
-    |> Map.put("data", JaSerializer.Formatter.format(struct.data))
+  @jsonapi_version "1.0"
+
+  def format(struct = %{errors: nil}) do
+    %{"data" =>  JaSerializer.Formatter.format(struct.data)}
     |> format_links(struct.links)
     |> Utils.put_if_present("meta", JaSerializer.Formatter.format(struct.meta))
     |> Utils.put_if_present("included", JaSerializer.Formatter.format(struct.included))
+    |> put_version
+  end
+
+  def format(struct = %{data: nil}) do
+    %{"errors" =>  JaSerializer.Formatter.format(struct.errors)} |> put_version
   end
 
   defp format_links(resource, nil), do: resource
   defp format_links(resource, []), do: resource
-
   defp format_links(resource, links) do
     links = links
             |> JaSerializer.Formatter.format
             |> Enum.reject(&(is_nil(&1)))
             |> Enum.into(%{})
     Map.put(resource, "links", links)
+  end
+
+  defp put_version(resource) do
+    resource |> Map.put("jsonapi", %{"version" => @jsonapi_version})
   end
 end
