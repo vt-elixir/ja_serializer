84ca7766262c2599800e5602a0d8cdaa3f5a5d22
Sina Karimi <creamycode@gmail.com>
2016-12-14 10:39:40 +1100

    Refactor function arguments

--- lib/ja_serializer/content_type_negotiation.ex
+++ lib/ja_serializer/content_type_negotiation.ex
@@ -32,7 +32,7 @@ defmodule JaSerializer.ContentTypeNegotiation do
   def verify_content_type(%Plug.Conn{method: "GET"} = conn, _o), do: conn
   def verify_content_type(%Plug.Conn{method: "DELETE"} = conn, _o), do: conn
   def verify_content_type(%Plug.Conn{} = conn, _o) do
-    if Enum.any?(get_req_header(conn, "content-type"), fn(x) -> String.match?(x, ~r/^application\/vnd.api\+json(;.*|;?)$/) end) do
+    if Enum.any?(get_req_header(conn, "content-type"), &String.match?(&1, ~r/^application\/vnd.api\+json(;.*|;?)$/)) do
       conn
     else
       halt send_resp(conn, 415, "")
