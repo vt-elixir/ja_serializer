8e0c9b9b229d1b323108e46bbaabd8893e8d6420
Shawn Liang <broodfusion@gmail.com>
2018-10-19 13:42:20 -0700

    refactor cond do to use pattern matching

--- lib/ja_serializer/formatter/attribute.ex
+++ lib/ja_serializer/formatter/attribute.ex
@@ -1,18 +1,12 @@
 defimpl JaSerializer.Formatter, for: JaSerializer.Builder.Attribute do
   alias JaSerializer.Formatter.Utils
 
+  def format(%{value: value, key: key}) when is_map(value) or is_list(value) do
+    values = Utils.deep_format_keys(value)
+    {Utils.format_key(key), JaSerializer.Formatter.format(values)}
+  end
+
   def format(attr) do
-    cond do
-      is_list(attr.value) ->
-        values = Utils.deep_format_keys(attr.value)
-        {Utils.format_key(attr.key), JaSerializer.Formatter.format(values)}
-
-      is_map(attr.value) ->
-        values = Utils.deep_format_keys(attr.value)
-        {Utils.format_key(attr.key), JaSerializer.Formatter.format(values)}
-
-      true ->
-        {Utils.format_key(attr.key), JaSerializer.Formatter.format(attr.value)}
-    end
+    {Utils.format_key(attr.key), JaSerializer.Formatter.format(attr.value)}
   end
 end
