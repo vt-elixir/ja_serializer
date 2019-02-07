38fc42d0fef163b17af9645a62f5232bef8ade7f
Bryan Naegele <bnaegele@weedmaps.com>
2019-02-07 11:29:50 -0700

    Restrict poison dep to only download for necessary envs

--- mix.exs
+++ mix.exs
@@ -25,7 +25,7 @@ defmodule JaSerializer.Mixfile do
     [
       {:inflex, "~> 1.4"},
       {:plug, "> 1.0.0"},
-      {:poison, ">= 1.4.0"},
+      {:poison, ">= 1.4.0", only: [:docs, :test]},
       {:ecto, "~> 1.1 or ~> 2.0", only: :test},
       {:earmark, "~> 0.1", only: :dev},
       {:inch_ex, "~> 0.4", only: :docs},
