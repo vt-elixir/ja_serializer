eef662e4998a51c399b950f6ee6e2ad6c7f469a1
David Brusius <david.brusius@magnetis.com.br>
2019-11-08 10:40:11 -0300

    Export `locals_without_parens` configuration on `formatter.exs`
    Export formatter configuration to allow clients import it as formatting
    options on their own `formatter.exs` file.
    
    This is done by adding the library name in the `import_deps` setting
    which will automatically inherit configurations that are defined inside the
    library `export` option.
    
    For more information, check [mix format docs](https://hexdocs.pm/mix/1.9.4/Mix.Tasks.Format.html#module-importing-dependencies-configuration)

--- .formatter.exs
+++ .formatter.exs
@@ -1,4 +1,10 @@
+locals_without_parens = [attributes: 1, location: 1, has_many: 2, has_one: 2]
+
 [
   inputs: ["*.{ex,exs}", "{bench,lib,test}/**/*.{ex,exs}"],
-  line_length: 80
+  line_length: 80,
+  locals_without_parens: locals_without_parens,
+  export: [
+    locals_without_parens: locals_without_parens
+  ]
 ]
