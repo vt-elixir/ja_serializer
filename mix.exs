446589508216ea341a24bd0b9d0d50ddaaab7722
Sam Hamilton <sam@sh81.com>
2020-05-18 22:33:19 +0800

    Bump Elixir version

--- mix.exs
+++ mix.exs
@@ -4,8 +4,8 @@ defmodule JaSerializer.Mixfile do
   def project do
     [
       app: :ja_serializer,
-      version: "0.15.0",
-      elixir: "~> 1.1",
+      version: "0.16.0",
+      elixir: "~> 1.7",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       consolidate_protocols: Mix.env() != :test,
